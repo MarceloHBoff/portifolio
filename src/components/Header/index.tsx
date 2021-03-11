b266db1e1f1610cdc815bd344cf94ef034c899c5
MarceloBoff <marceloboff20@gmail.com>
2021-03-11 19:41:53 -0300

    Custom header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,27 @@
+import { useRouter } from 'next/dist/client/router'
+import Link from 'next/link'
+
 import Logo from '../../../public/logo.svg'
 
 import { Container, Menu, Menus } from './styles'
 
 const Header = () => {
+  const router = useRouter()
+
   return (
     <Container>
       <Logo />
 
       <Menus>
-        <Menu>Projetos</Menu>
-        <Menu>Sobre</Menu>
-        <Menu>Contato</Menu>
+        <Link href="/">
+          <Menu selected={router.pathname === '/'}>Projetos</Menu>
+        </Link>
+        <Link href="/whoIAm">
+          <Menu selected={router.pathname === '/whoIAm'}>Sobre</Menu>
+        </Link>
+        <Link href="/contact">
+          <Menu selected={router.pathname === '/contact'}>Contato</Menu>
+        </Link>
       </Menus>
     </Container>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
   background-color: ${props => props.theme.colors.header};
@@ -25,4 +25,36 @@ export const Menus = styled.div`
   width: 900px;
 `
 
-export const Menu = styled.div``
+export const Menu = styled.button<{ selected: boolean }>`
+  font-size: ${props => props.theme.font.sizes.xlarge};
+
+  color: ${props => props.theme.colors.text};
+  padding: 5px 10px;
+
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border-radius: 10px;
+    border-bottom: 2px solid ${props => props.theme.colors.text};
+    transition: transform 0.3s ease-in-out;
+    transform: scaleX(0);
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
+
+  ${props =>
+    props.selected &&
+    css`
+      &::after {
+        transform: scaleX(1);
+        border-color: ${props => props.theme.colors.primary};
+      }
+    `}
+`
