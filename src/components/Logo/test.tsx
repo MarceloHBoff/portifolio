4ca9065d2edc5314ca2b76094356268af2f8d441
MarceloBoff <marceloboff20@gmail.com>
2021-03-25 22:20:50 -0300

    Add logo component

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render correctly', () => {
+    const { container } = render(<Logo />)
+
+    const logo = screen.getByRole('img', { name: /marcelo boff/i })
+    expect(logo).toBeInTheDocument()
+    expect(logo.parentElement).toHaveStyle({
+      width: '32rem',
+      height: '5rem'
+    })
+
+    expect(container.parentElement).toMatchSnapshot()
+  })
+
+  it('should render logo with small size', () => {
+    render(<Logo size="small" />)
+
+    const logo = screen.getByRole('img', { name: /marcelo boff/i })
+    expect(logo.parentElement).toHaveStyle({
+      width: '24rem',
+      height: '3.5rem'
+    })
+  })
+})
